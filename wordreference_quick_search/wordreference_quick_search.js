802c72684cc65d909318aa782316892e31bb65d8
mpascucci <marpas.paris@gmail.com>
2020-11-18 12:58:13 +0100

    clean wordreference extension

--- wordreference_quick_search/wordreference_quick_search.js
+++ wordreference_quick_search/wordreference_quick_search.js
@@ -1,35 +1,62 @@
-/* Wordreferencei quick search add-on extension for Firefox
+/* Wordreference quick search add-on extension for Firefox
  *
  * Allows quick searching wordreference from the address bar
  *
- * Usage:
- * 	defalt search: wr <search string>
+ * Usage (in the address bar):
+ * 	wr -[from][to] <search string>
+ *      
+ *      [from][to] are two letter language codes (e.g. "en" for english)
+ *      this parameters allow to override the application settings
+ *      which are used as default values otherwise.
  */
 
 browser.omnibox.setDefaultSuggestion({
   description: "Search WordReference"
 });
 
-console.log("base level");
+/* Parse omnibox user input */
+function parseUserInput(input) {
+  var language;
+  var split_input = input.split(" ",2);
+  // detect option starting with minus
+  if ((split_input.length==2) && (split_input[0].startsWith('-'))) {
+    // detect language options
+    language = {
+      from: split_input[0].slice(1,3),
+      to: split_input[0].slice(3,5)
+    };
+    input = split_input[1];
+  }; 
+  return {language:language, text:input};
+}
 
 browser.omnibox.onInputEntered.addListener((input, disposition) => {
-  console.log("ciccio");
-  browser.storage.sync.get(["langFrom","langTo"]).then( (item) =>  {
+    browser.storage.sync.get(["langFrom","langTo"]).then( (item) =>  {
+        // chech if user specified parameters in the omnibox.
+        // in this case override the extension settings.
+        params = parseUserInput(input);
+        if (params.language) {
+            item.langFrom = params.language.from;
+            if (params.language.to) {
+              item.langTo = params.language.to;
+            }
+          input = params.text;
+        }
         var url = `https://www.wordreference.com/${item.langFrom}${item.langTo}/${input}`;
         switch (disposition) {
           case "currentTab":
             browser.tabs.update({url});
-            break;
+          break;
           case "newForegroundTab":
             browser.tabs.create({url});
             break;
           case "newBackgroundTab":
             browser.tabs.create({url, active: false});
-            break;
+          break;
         }
       },
       (error) => {
-        console.log(`Error: ${error}`); 
-      }
-    );
+      console.log(`Error: ${error}`); 
+    }
+  );
 });
